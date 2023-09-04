c615ba7638f0ebf46a4b94ccf19b25d581d4d636
LilianScarabello <lilianscarabello@gmail.com>
2023-09-04 10:00:25 -0300

    Foi inserida a imagem do card e a logo

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+console.log(data);
 
-console.log(example, data);
+for (let i = 0; i < data.length; i++) {
+  const template = `<img src="${data.pokemon[i].img}">`;
+  const cardContainer = document.getElementById("cardsContainer");
+  cardContainer.innerHTML += template;
+}
+
+
