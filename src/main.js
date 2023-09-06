9c59fed9407ae8ca21e2a9c2eaa042497d326f51
LilianScarabello <lilianscarabello@gmail.com>
2023-09-06 20:56:55 -0300

    foi incluido os cards

--- src/main.js
+++ src/main.js
@@ -4,8 +4,18 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 console.log(data);
 
-for (let i = 0; i < data.length; i++) {
-  const template = `<img src="${data.pokemon[i].img}">`;
+
+for (let i = 0; i < data.pokemon.length; i++) {
+  const template = `
+  <figure>
+    <img src="${data.pokemon[i].img}"> 
+    <p>
+       Num:${data.pokemon[i].num}
+       Name:${data.pokemon[i].name}
+       Type:${data.pokemon[i].type}
+       About:${data.pokemon[i].about}
+    </p> 
+  </figure>`;
   const cardContainer = document.getElementById("cardsContainer");
   cardContainer.innerHTML += template;
 }
