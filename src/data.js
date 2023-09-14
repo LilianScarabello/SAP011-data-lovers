14c1634608f412a01dc91a4cfb8cd9142bbb0e46
LilianScarabello <lilianscarabello@gmail.com>
2023-09-14 14:08:12 -0300

    funções de filtrar e ordenação.

--- src/data.js
+++ src/data.js
@@ -1,9 +1,18 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+export function filtrarResultados(pokemons, type) {
+  const pokemonsFiltrados = pokemons.filter(pokemon => (pokemon.type.includes(type)));
+  return pokemonsFiltrados;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export function resultadoPesquisa(pokemons, name) {
+  const sugestaoResultado = pokemons.filter(pokemon => (pokemon.name.includes(name.toLowerCase())));
+  return sugestaoResultado;
+};
+
+export function ordenacao(pokemon,ordenarAZ) {
+  pokemon.sort((a, b) => a.name.localeCompare(b.name));
+  if (ordenarAZ.value = "A - Z") {
+    return pokemon;
+  } else {
+    return pokemon.reverse();
+  }
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
+import { filtrarResultados, resultadoPesquisa, ordenacao } from './data.js';
+import allPokemons from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 console.log(data);
 
@@ -10,14 +10,80 @@ for (let i = 0; i < data.pokemon.length; i++) {
   <figure>
     <img src="${data.pokemon[i].img}"> 
     <p>
-       Num:${data.pokemon[i].num}
-       Name:${data.pokemon[i].name}
-       Type:${data.pokemon[i].type}
-       About:${data.pokemon[i].about}
+       Num:${data.pokemon[i].num}<br>
+       Name:${data.pokemon[i].name}<br>
+       Type:${data.pokemon[i].type}<br>
+       About:${data.pokemon[i].about}<br>
     </p> 
   </figure>`;
   const cardContainer = document.getElementById("cardsContainer");
   cardContainer.innerHTML += template;
 }
 
+const cardsType = document.getElementById("typePokemon");
+cardsType.addEventListener("change", () => {
+  const type = cardsType.value;
+  console.log(type)
+  const resultado = filtrarResultados(data.pokemon, type);
+  console.log(resultado)
+  const cardContainer = document.getElementById("cardsContainer");
+  cardContainer.innerHTML = "";
+  for (let i = 0; i < resultado.length; i++) {
+    const template = `
+    <figure>
+      <img src="${resultado[i].img}"> 
+      <p>
+         Num:${resultado[i].num}<br>
+         Name:${resultado[i].name}<br>
+         Type:${resultado[i].type}<br>
+         About:${resultado[i].about}<br>
+      </p> 
+    </figure>`;
+    cardContainer.innerHTML += template;
+  }
+})
+const cardsName = document.querySelector(".search");
+function buscarCard() {
+  const name = cardsName.value;
+  console.log(name)
+  const resultadoPesq = resultadoPesquisa(data.pokemon, name);
+  console.log(resultadoPesq)
+  const cardContainer = document.getElementById("cardsContainer");
+  cardContainer.innerHTML = "";
+  for (let i = 0; i < resultadoPesq.length; i++) {
+    const template = `
+    <figure>
+      <img src="${resultadoPesq[i].img}"> 
+      <p>
+         Num:${resultadoPesq[i].num}<br>
+         Name:${resultadoPesq[i].name}<br>
+         Type:${resultadoPesq[i].type}<br>
+         About:${resultadoPesq[i].about}<br>
+      </p> 
+    </figure>`;
+    cardContainer.innerHTML += template;
+  }
+};
+cardsName.addEventListener("input", buscarCard);
 
+const ordenarAZ = document.querySelector("#nameSort");
+ordenarAZ.addEventListener("click", function (e) {
+  e.preventDefault();
+  const newPokemon = allPokemons.pokemon;
+  const alphaResults = ordenacao(newPokemon,ordenarAZ);
+  const cardContainer = document.getElementById("cardsContainer");
+  cardContainer.innerHTML = "";
+  for (let i = 0; i < alphaResults.length; i++) {
+    const template = `
+  <figure>
+    <img src="${alphaResults[i].img}"> 
+    <p>
+       Num:${alphaResults[i].num}<br>
+       Name:${alphaResults[i].name}<br>
+       Type:${alphaResults[i].type}<br>
+       About:${alphaResults[i].about}<br>
+    </p> 
+  </figure>`;
+    cardContainer.innerHTML += template;
+  }
+});
